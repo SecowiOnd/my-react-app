f423d611e95bb8d6fbb81a1d91f56fe75ab9d66d
Secowi <myEmail@mail.ru>
2020-05-13 04:17:08 +0300

    Updates

---
+++ precache-manifest.2a82eb5af3a72a3caee8c50a8f17fd24.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24c2c858ac70b3b9427e5675fe4ab80a",
+    "url": "/my-react-app/index.html"
+  },
+  {
+    "revision": "c04bcab7dbfe430b8fc8",
+    "url": "/my-react-app/static/css/3.dfd79c08.chunk.css"
+  },
+  {
+    "revision": "206d4ac6aab362fd4c8c",
+    "url": "/my-react-app/static/css/4.515eb8cc.chunk.css"
+  },
+  {
+    "revision": "666c476afebdc7260fab",
+    "url": "/my-react-app/static/css/main.97dc427f.chunk.css"
+  },
+  {
+    "revision": "8102907a1cd9391e1112",
+    "url": "/my-react-app/static/js/2.38861a00.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/my-react-app/static/js/2.38861a00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c04bcab7dbfe430b8fc8",
+    "url": "/my-react-app/static/js/3.0a59aefc.chunk.js"
+  },
+  {
+    "revision": "206d4ac6aab362fd4c8c",
+    "url": "/my-react-app/static/js/4.df7e9e40.chunk.js"
+  },
+  {
+    "revision": "666c476afebdc7260fab",
+    "url": "/my-react-app/static/js/main.a29e26fa.chunk.js"
+  },
+  {
+    "revision": "313674ec2a024851b326",
+    "url": "/my-react-app/static/js/runtime-main.d07de83d.js"
+  },
+  {
+    "revision": "21fc7d1b74aef42eb6d9a65d73c4bb39",
+    "url": "/my-react-app/static/media/index.21fc7d1b.gif"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-react-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a5fafe5de16aab6b2587884a26b7295c",
+    "url": "/my-react-app/static/media/user.a5fafe5d.png"
+  }
+]);
